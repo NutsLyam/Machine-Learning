23b8d76fa1fc0907cbb777bf1ccb109ad8443de6
NutsLyam <natalyailyamakova@yandex.ru>
2019-01-20 22:00:23 +0300

    Add files via upload

--- Affinity-propagation/Functions.h
+++ Affinity-propagation/Functions.h
@@ -7,93 +7,122 @@
 #include <sstream>
 #include <ctime> 
 #include <cstdlib>
+#include <limits>
+
 
 using namespace std;
 struct Edge
 {
 	int from;
 	int to;
-	float s = 1;
-	float a = 0;
-	float r = 0;
-	Edge(int from, int to, float s, float a, float r) :
+	double s = 1;
+	double a = 0;
+	double r = 0;
+	Edge(int from, int to, double s, double a, double r) :
 		from(from), to(to), s(s), a(a), r(r)
 	{}
 };
 
-float alfa = 0.6;
+double alfa = 0.6, N = 196591;
+clock_t start, finish;
 
 using Vector_of_edges = vector<Edge*>;
 //split line 
 template <class Container>
 void split1(const std::string& str, Container& cont, char delim = '\t')
 {
-	std::stringstream ss(str);
-	std::string token;
+	stringstream ss(str);
+	string token;
 	cont.clear();
-	while (std::getline(ss, token, delim)) {
+	while (getline(ss, token, delim)) {
 		cont.push_back(token);
 	}
 }
 
-
-float exponential_smoothing(float prev_value, float next_value, float alfa)
+void read(vector<Vector_of_edges> from_edge, vector<Vector_of_edges> to_edge, vector<Edge*> diagonal)
+{
+	srand(time(NULL));
+	vector<string> coordinates; // [from, to]
+	string line;
+	ifstream in;
+	int x, y, N = 196591;
+	int counter = 0;
+	in.open("C:\\Users\\Nuts\\Documents\\Visual Studio 2015\\Projects\\AffinityPropagation\\Gowalla_edges.txt");
+	if (in.is_open())
+	{
+		cout << "Begin!!" << endl;
+		//counter = 0;
+		start = clock();
+		while (getline(in, line)) //&& counter < 1000) 
+		{
+			split1(line, coordinates); // split
+			x = stoi(coordinates[0]);
+			y = stoi(coordinates[1]);
+			//cout << x << "  "  << y << endl;
+			double press = 0.00001 * (rand() % 101);
+			Edge *edge = new Edge(x, y, 1 + press, 0, 0); //pointer at the new edge, указатель  на новую считанную вершину
+			from_edge[x].push_back(edge);
+			to_edge[y].push_back(edge);
+			//counter++;
+		}
+		finish = clock();
+		cout << "Time of reading : " << (double)(finish - start) / ((double)CLOCKS_PER_SEC) << endl;
+		in.close();
+	}
+ //diagonal
+	for (int i = 0; i < N; i++)
+	{
+		double press = 0.00001 * (rand() % 101);
+		Edge *edge = new Edge(i, i, -3 - press, 0, 0);
+		diagonal.push_back(edge);
+	}
+}
+double exponential_smoothing(double prev_value, double next_value, double alfa)
 {
 	return alfa * next_value + (1 - alfa) * prev_value;
 }
 
-void find_max12(Vector_of_edges row, float * max1, float * max2, int * max_index)
+void find_max12(Vector_of_edges row, double * max1, double * max2, int * max_index)
 {
-	float value;
-	for (int j = 0; j < row.size(); j++)
-	{
-		value = row[j]->a + row[j]->s;
-		if (value > *max1)
+	//cout << row.size() << endl;
+	double value;
+	if (row.size() > 1) {
+		for (int j = 0; j < row.size(); j++)
 		{
-			*max1 = value;
-			*max_index = j;
+			value = row[j]->a + row[j]->s;
+			if (value > *max1)
+			{
+				*max2 = *max1;
+				*max1 = value;
+				*max_index = j;
+			}
+			else if (value > *max2)
+			{
+				*max2 = value;
+			}
 		}
-		else if (value > *max2)
-		{
-			*max2 = value;
-		}
+	}
+	//
+	else {
+		value = row[0]->a + row[0]->s;
+		*max2 = *max1 = value;
+		*max_index = 0;
 	}
 }
-//compare with diaginal elements
-void cheak_diag(Edge * element, float*max1, float * max2, int*max_index, bool*flag, int _size)
-{
-	float value = element->a + element->s;
-	if (value > *max1)
-	{
-		*max2 = *max1;
-		*max1 = value;
-		*max_index = _size;
-		element->r = exponential_smoothing(element->r, element->s - *max2, alfa);
-		*flag = true;  // true if diagonal element is changed
-	}
-	else if (value > *max2)
-	{
-		*max2 = value;
-		//element->r = element->s - *max1;
-		//*flag = true;
-	}
-}
-
 void update_R(vector<Vector_of_edges> from_edge, vector<Edge*> diagonal)
 {
 	for (int i = 0; i < from_edge.size(); i++)
 	{
 		Vector_of_edges  row = from_edge[i];
-		float max1 = -10000;
-		float max2 = -10000;
-		float value, new_r;
+		
+		double max1 = -numeric_limits<double>::infinity();
+		double max2 = -numeric_limits<double>::infinity();
+		double value, new_r;
 		int max_index = 0;
-		bool diag_chanded = false;
 		//
-		find_max12(row, &max1, &max2, &max_index);
-		cheak_diag(diagonal[i], &max1, &max2, &max_index, &diag_chanded, from_edge[i].size());
 
-		//cout << from_edge[i].size() << endl;
+		find_max12(row, &max1, &max2, &max_index);
+	
 		for (int k = 0; k < max_index; k++)
 		{
 			new_r = from_edge[i][k]->s - max1;
@@ -106,13 +135,10 @@ void update_R(vector<Vector_of_edges> from_edge, vector<Edge*> diagonal)
 			from_edge[i][k]->r = exponential_smoothing(from_edge[i][k]->r, new_r, alfa);
 		}
 
+		diagonal[i]->r = exponential_smoothing(diagonal[i]->r, diagonal[i]->s - max1, alfa);
+		new_r = from_edge[i][max_index]->s - max2;
+		from_edge[i][max_index]->r = exponential_smoothing(from_edge[i][max_index]->r, new_r, alfa);
 
-		if (!diag_chanded)
-		{
-			diagonal[i]->r = exponential_smoothing(diagonal[i]->r, diagonal[i]->s - max1, alfa);
-			new_r = from_edge[i][max_index]->s - max2;
-			from_edge[i][max_index]->r = exponential_smoothing(from_edge[i][max_index]->r, new_r, alfa);
-		}
 	}
 }
 void update_A(vector<Vector_of_edges> to_edge, vector<Edge*> diagonal)
@@ -121,10 +147,11 @@ void update_A(vector<Vector_of_edges> to_edge, vector<Edge*> diagonal)
 	{
 		{
 			auto col = to_edge[k];
-			float new_a, sum = 0;
+			double new_a, sum = 0;
 			for (int j = 0; j < col.size(); j++)
 			{
 				sum += (col[j]->r > 0) * col[j]->r;
+	
 			}
 			for (int j = 0; j < to_edge[k].size(); j++)
 			{
@@ -136,4 +163,39 @@ void update_A(vector<Vector_of_edges> to_edge, vector<Edge*> diagonal)
 			diagonal[k]->a = exponential_smoothing(diagonal[k]->a, sum, alfa);
 		}
 	}
+}
+void choose_cluster(vector<Vector_of_edges> from_edge, vector<Edge*> diagonal)
+{
+	ofstream fout;
+	vector<int> c(N);
+	for (size_t i = 0; i < from_edge.size(); i++)
+	{
+		auto row = from_edge[i];
+		double value, max = -numeric_limits<float>::infinity();;
+		int index = 0;
+		for (size_t j = 0; j < row.size(); j++)
+		{
+			value = row[j]->a + row[j]->r;
+			if (value > max)
+			{
+				max = value;
+				index = row[j]->to;
+			}
+		}
+
+		// проверка диагональных элементов
+		if (diagonal[i]->a + diagonal[i]->r > max)
+		{
+			index = diagonal[i]->to;
+		}
+		c[i] = index;
+
+	}
+	//write labels
+	fout.open("fout_exemplars.txt");
+	for (int i = 0; i < N; i++)
+	{
+		fout << i << " \t" << c[i] << endl;
+	}
+	fout.close();
 }
--- Affinity-propagation/affinity_propagation.cpp
+++ Affinity-propagation/affinity_propagation.cpp
@@ -10,101 +10,78 @@
 #include "Functions.h"
 
 using namespace std;
+//choose cluster labels
+
 
 int main() {
-	clock_t start, end;
+	int iteration = 0, Maxiteration = 600;
+	
 
-	ifstream in;          
-	in.open("C:\\Users\\Nuts\\Documents\\Visual Studio 2015\\Projects\\AffinityPropagation\\Gowalla_edges.txt"); 
-	
-	vector<string> coordinates; // [from, to]
-	string line;
-	int x, y, N = 196591; 
-	int iteration = 0, Maxiteration = 500, counter = 0;
-	vector<int> c(N);
-	
 	using Vector_of_edges = vector<Edge*>; //вектор из указателей на вершины
 	vector<Vector_of_edges> from_edge(N); // row, вектор из векторов
 	vector<Vector_of_edges> to_edge(N); // column
 	vector<Edge*> diagonal;
 
-	//вывод в файл
-	ofstream fout;
+	/*x
 	srand(time(NULL));
-	
-	// reading and filling 
+	vector<string> coordinates; // [from, to]
+	string line;
+	ifstream in;
+	int x, y, N = 196591;
+	int counter = 0;
+	in.open("C:\\Users\\Nuts\\Documents\\Visual Studio 2015\\Projects\\AffinityPropagation\\Gowalla_edges.txt");
 	if (in.is_open())
 	{
 		cout << "Begin!!" << endl;
 		//counter = 0;
 		start = clock();
-		while (getline(in, line)) //counter < 10) 
+		while (getline(in, line)) //&& counter < 1000) 
 		{
 			split1(line, coordinates); // split
-			x = stoi(coordinates[0]) ;
+			x = stoi(coordinates[0]);
 			y = stoi(coordinates[1]);
 			//cout << x << "  "  << y << endl;
-			float press = 0.0001 * (rand() % 101);
-			Edge *edge = new Edge(x,y,1 + press,0,0); //pointer at the new edge, указатель  на новую считанную вершину
+			double press = 0.0001 * (rand() % 101);
+			Edge *edge = new Edge(x, y, 1 + press, 0, 0); //pointer at the new edge, указатель  на новую считанную вершину
 			from_edge[x].push_back(edge);
 			to_edge[y].push_back(edge);
 			//counter++;
 		}
-		end = clock();
-		cout << "Time of reading : " << (double)(end - start) / ((double)CLOCKS_PER_SEC) << endl;
+		finish = clock();
+		cout << "Time of reading : " << (double)(finish - start) / ((double)CLOCKS_PER_SEC) << endl;
 		in.close();
+
+		for (int i = 0; i < N; i++)
+		{
+			double press = 0.0001 * (rand() % 101);
+			Edge *edge = new Edge(i, i, -1 - press, 0, 0);
+			diagonal.push_back(edge);
+		}
 	}
-	for (int i = 0; i < N; i++)
-	{
-		float press = 0.0001 * (rand() % 101);
-		Edge *edge = new Edge(i, i, -1+press, 0, 0);
-		diagonal.push_back(edge);
-	}
-	while (iteration < Maxiteration) 
+*/
+read(from_edge, to_edge, diagonal);
+
+	// reading and filling 
+
+	while (iteration < Maxiteration)
 	{
 		iteration++;
-	// update matrix R
-	 start = clock();
-	update_R(from_edge, diagonal);
-	
-	//matrix A
-	update_A(to_edge, diagonal);
+		// update matrix R
+		start = clock();
+		update_R(from_edge, diagonal);
 
-	end = clock();
-	cout << iteration<< " iteration : " << (double)(end - start) / ((double)CLOCKS_PER_SEC) << " s" << endl;
-	}
+		//matrix A
+		update_A(to_edge, diagonal);
 
-//choose cluster labels
-	for (size_t i = 0; i < from_edge.size(); i++)
-	{
-		auto row = from_edge[i];
-		float value, max = -10000;
-		int index = 1;
-		for (size_t j = 0; j < row.size(); j++)
+		finish = clock();
+		cout << iteration << " iteration : " << (double)(finish - start) / ((double)CLOCKS_PER_SEC) << " s" << endl;
+
+		if (iteration%50==0)
 		{
-			value = row[j]->a + row[j]->r;
-			if (value > max)
-			{
-				max = value;
-				index = row[j]->to;
-			}
+			choose_cluster(from_edge, diagonal);
 		}
-		// проверка диагональных элементов
-		if (diagonal[i]->a + diagonal[i]->r > max)
-		{
-			index = diagonal[i]->to;
-		}
-		c[i] = index;
 	}
-
-//write labels
-	fout.open("fout_exemplars.txt");
-	for (int i = 0; i < N; i++)
-	{
-		fout << i << " \t" << c[i] << endl;
-	}
-	fout.close();
-
+	choose_cluster(from_edge, diagonal);
 	system("pause");
 	return 0;
 }
